7b44c0b02fe22b54c30f4468609e28b811e8e52f
Dustin Schaerer <dustinschaerer@gmail.com>
2016-04-29 12:37:11 -0700

    Active admin Post tweaks

--- app/admin/meat.rb
+++ app/admin/meat.rb
@@ -2,8 +2,4 @@ ActiveAdmin.register Meat do
 
   permit_params :name, :price, :description, :sort
 
-  # controller do
-  #   cache_sweeper :menu_sweeper
-  # end
-
 end
--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,19 +1,15 @@
 ActiveAdmin.register Post do
-  belongs_to :admin_user
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :content, :admin_user_id
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
 
+  permit_params :title, :content, :admin_user_id
 
+  form do |f|
+    f.inputs "Post Details" do
+      f.input :admin_user_id
+      f.input :title
+      f.input :content
+    end
+    f.actions
+  end
 
 end
